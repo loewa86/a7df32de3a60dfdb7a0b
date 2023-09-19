b160f07029ef425062b2fd3add7541d70e2aae6d
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-09-19 12:40:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="a7df32de3a60dfdb7a0b",
-    version="0.10.450",
+    version="0.10.500",
     packages=find_packages(),
     install_requires=[
         "snscrape",
